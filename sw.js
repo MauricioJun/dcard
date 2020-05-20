5652077ee73e16c11c6480ce64f9092697a6c7c4
MauricioJun <64089786+MauricioJun@users.noreply.github.com>
2020-05-20 14:45:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -64,8 +64,8 @@ self.addEventListener('fetch', function(event) {
 				return response
 			});
 		}).catch(function() {
-		// If both fail, show a generic fallback:
-		return caches.match('/dcard/offline.html');
+			// If both fail, show a generic fallback:
+			return caches.match('/dcard/offline.html');
 		})
 	);
 });
@@ -74,7 +74,7 @@ self.addEventListener('fetch', function(e) {
 	console.log('e.request.url 01 = ' + e.request.url);
 	e.respondWith(
 		caches.match(e.request).then(function(response) {
-			console.log('response = ' + e.request.url);
+			console.log('response = ' + response);
 			console.log('e.request.url 02 = ' + e.request.url);
 			return response || fetch(e.request);
 		})
